df82e41d752d819a8ef2c6beeefaa1cdab568202
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-05-29 02:05:50 +0900

    fix: トップページ、ログイン画面、新規登録画面でヘッダーと下ナビが非表示になるように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def show_navigation?
+  logged_in? && !hide_navigation?
+  end
+
+  private
+
+  def hide_navigation?
+    static_top_page? || auth_page?
+  end
+
+  def static_top_page?
+    controller_name == "static_pages" && action_name == "top"
+  end
+
+  def auth_page?
+    (controller_name == "sessions" && action_name.in?(%w[new create])) ||
+      (controller_name == "users" && action_name.in?(%w[new create]))
+  end
 end
